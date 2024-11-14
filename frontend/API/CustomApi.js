93ba140ddb3eee614c6166896e47fb857f329cf9
AbdullahMukadam <abdullahmukadam21@gmail.com>
2024-11-14 07:48:16 -0800

    fixing the cookies

--- frontend/API/CustomApi.js
+++ frontend/API/CustomApi.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { Config } from './Config';
 
-
 const api = axios.create({
   baseURL: Config.baseUrl,
-  withCredentials: Config.baseUrl.startsWith('http') && new URL(Config.baseUrl).origin !== window.location.origin
+  withCredentials: true,
 });
 
 export default api;
