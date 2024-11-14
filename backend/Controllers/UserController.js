4854b5429ddcae2144e94eff71fd5b010eae5a3c
AbdullahMukadam <abdullahmukadam21@gmail.com>
2024-11-14 08:03:27 -0800

    fixing

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -65,6 +65,7 @@ const Login = async (req, res) => {
       res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
         maxAge: 30 * 24 * 60 * 60 * 1000,
       })
         .status(200).json({
