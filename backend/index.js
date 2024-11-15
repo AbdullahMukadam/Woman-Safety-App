4df024d176a4599c8c9cdad5f710ed2268f72baa
AbdullahMukadam <abdullahmukadam21@gmail.com>
2024-11-15 05:06:05 -0800

    still fixing

--- backend/index.js
+++ backend/index.js
@@ -12,17 +12,23 @@ import cors from "cors"
 dotenv.config()
 const app = express()
 
+const corsOptions = {
+  origin: process.env.NODE_ENV === "production" 
+    ? 'https://woman-safety-app-front.vercel.app'
+    : ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['set-cookie'],
+  preflightContinue: true,
+};
 
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.use(cors({
-  origin: process.env.NODE_ENV === "production" 
-    ? 'https://woman-safety-app-front.vercel.app'
-    : 'http://localhost:5173',
-  credentials: true
-}));
+
 
 ConnectToDb();
 
