4577b3d291579921db5b8c847e754b290749b671
AbdullahMukadam <abdullahmukadam21@gmail.com>
2025-04-20 08:57:42 -0700

    updated the url for cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const _dirname = path.resolve();
 
 const corsOptions = {
   origin: process.env.NODE_ENV === "production"
-    ? 'https://woman-safety-app.onrender.com'
+    ? 'https://woman-safety-app.vercel.app'
     : ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
